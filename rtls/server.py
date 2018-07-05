f6a6776f9a2383b62d254476feb182b869bdbd58
Petri Mattila <petri.mattila@unipart.io>
2018-07-05 19:42:54 +0100

    [rtls] First working frequency and delay estimator

--- rtls/server.py
+++ rtls/server.py
@@ -3,73 +3,86 @@
 # Test hack for developing RPi Tail algorithms
 #
 
-import pprint
 import argparse
-import ipaddress
-import netifaces
 import socket
-import select
-import ctypes
-import json
+import pprint
+import math
+import tail
 
 
 class Config():
-    remote     = None
+
+    blinks     = 100
+    
     rpc_port   = 61666
     rpc_addr   = None
-    rpc_dest   = None
-    rpc_bind   = None
+
+    dw1000_attrs = (
+        'snr_threshold',
+        'fpr_threshold',
+        'noise_threshold',
+        'channel',
+        'pcode',
+        'txpsr',
+        'prf',
+        'rate',
+        'antd',
+        'xtalt',
+        'smart_power',
+    )
+
 
 cfg = Config()
 
 
-
-def SendRPC(sock,func,args):
-    data = {
-        'func': func,
-        'args': args,
-    }
-    msg = json.dumps(data)
-    sock.sendto(msg.encode(), cfg.rpc_dest)
-
-
-def RecvRPC(sock):
-    (data, client) = sock.recvfrom(4096)
-    try:
-        rpc = json.loads(data.decode())
-    except:
-        rpc = { }
-    pprint.pprint(rpc)
-    
-    
-def SocketLoop():
-    rsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    rsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    rsock.bind(cfg.rpc_bind)
-
-    while True:
-        rset,wset,eset = select.select([rsock],[],[])
-        if rsock in rset:
-            RecvRPC(rsock)
-
-    
 def main():
-    global cfg
     
-    parser = argparse.ArgumentParser(description="Anchor daemon")
+    parser = argparse.ArgumentParser(description="RTLS server")
     
+    parser.add_argument('-n', '--blinks', type=int, default=cfg.blinks)
     parser.add_argument('-p', '--port', type=int, default=cfg.rpc_port)
-    parser.add_argument('remote', type=str, help="Remote address")
+    parser.add_argument('remotes', type=str, nargs='+', help="Remote addresses")
     
     args = parser.parse_args()
 
-    cfg.rpc_addr = args.remote
-    cfg.rpc_port = args.port
-    cfg.rpc_dest = (cfg.rpc_addr, cfg.rpc_port)
-    cfg.rpc_bind = ('', cfg.rpc_port)
-
-    SocketLoop()
+    remotes = [ ]
     
+    for remote in args.remotes:
+        addr = socket.getaddrinfo(remote, args.port, socket.AF_INET6)[0][4]
+        remotes.append( { 'host': remote, 'addr': addr, 'EUI': None } )
+
+    rpc_addr = [ rem['addr'] for rem in remotes ]
+    rpc_bind = ('', args.port)
+
+    rpc = tail.RPC(rpc_bind)
+    blk = tail.Blinker(rpc)
+
+    for addr in rpc_addr:
+        rpc.setAttr(addr, 'txpsr', 256)
+    
+    for remote in remotes:
+        addr = remote['addr']
+        remote['EUI'] = rpc.getEUI(addr)
+        print('DW1000 parameters @{} <{}>'.format(remote['host'],remote['EUI']))
+        for attr in cfg.dw1000_attrs:
+            val = rpc.getAttr(addr, attr)
+            print('  {}={}'.format(attr, val))
+
+    timer = tail.Timer()
+    
+    for i in range(args.blinks):
+        for addr in rpc_addr:
+            blk.Blink(addr)
+            timer.nap(0.01)
+
+    blk.stop()
+    rpc.stop()
+
+    ##
+    ## Add analysis code here
+    ##
+
+
 
 
 if __name__ == "__main__":
