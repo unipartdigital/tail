5a8d2250c4e4c38130a1ea748b4e1f939972336d
Petri Mattila <petri.mattila@unipart.io>
2019-01-29 01:15:56 +0000

    rtls: More clever rfatten script
    Signed-off-by: Petri Mattila <petri.mattila@unipart.io>

--- rtls/rfatten.py
+++ rtls/rfatten.py
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plot
 
 class Config():
 
-    Ch     = 7
-    Dist   = 5.34
-    Power  = -75
+    channel    = 3
+    power      = -77
+    dist       = 7.24
 
 
 CFG = Config()
@@ -37,25 +37,25 @@ def ADI(dBm,fc,pt):
     return 10**((pt-dBm)/20)/(CC*fc*1e6)
 
 
-def CalcAtten(ch,dist,dBm):
+def CalcTxPower(ch,dist,dBm):
     return dBm - DAI(dist,FC[ch],0)
 
 
-def Plot(Ch=7, Pt=0.0, Range=None):
+def Plot(Ch=7, Ptx=0.0, Range=None):
 
     if Range is None:
         Range = ( 1, 100 )
 
     X = np.linspace(Range[0],Range[1],100)
-    Y = DAI(X,FC[Ch],Pt)
+    Y = DAI(X,FC[Ch],Ptx)
     
     YY = np.linspace(np.min(Y),np.max(Y),100)
-    XX = ADI(YY,FC[Ch],Pt)
+    XX = ADI(YY,FC[Ch],Ptx)
     
     
     fig,ax = plot.subplots(1,2,figsize=(20,10),dpi=120)
     
-    ax[0].set_title('Ch{} Pt:{:.1f}dBm'.format(Ch,Pt))
+    ax[0].set_title('Ch{} Ptx:{:.1f}dBm'.format(Ch,Ptx))
     ax[0].set_ylabel('[dBm]')
     ax[0].set_xlabel('[m]')
     ax[0].grid(True)
@@ -64,12 +64,12 @@ def Plot(Ch=7, Pt=0.0, Range=None):
     ax[0].plot(XX,YY,'-')
     
     
-    Y1 = DAI(X,FC[1],Pt)
-    Y2 = DAI(X,FC[2],Pt)
-    Y3 = DAI(X,FC[3],Pt)
-    Y4 = DAI(X,FC[4],Pt)
-    Y5 = DAI(X,FC[5],Pt)
-    Y7 = DAI(X,FC[7],Pt)
+    Y1 = DAI(X,FC[1],Ptx)
+    Y2 = DAI(X,FC[2],Ptx)
+    Y3 = DAI(X,FC[3],Ptx)
+    Y4 = DAI(X,FC[4],Ptx)
+    Y5 = DAI(X,FC[5],Ptx)
+    Y7 = DAI(X,FC[7],Ptx)
     
     ax[1].set_title('Channels')
     ax[1].set_ylabel('[dBm]')
@@ -96,35 +96,42 @@ def main():
     parser = argparse.ArgumentParser(description="RF propagation analysis")
 
     parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase verbosity')
-    parser.add_argument('-c', '--channel', type=int, default=CFG.Ch, help='channel')
-    parser.add_argument('-d', '--distance', type=float, default=CFG.Dist, help='distance')
-    parser.add_argument('-P', '--power', type=float, default=CFG.Power, help='power')
-    parser.add_argument('-C', '--cal', type=float, default=None)
+    parser.add_argument('-c', '--channel', type=int, default=CFG.channel, help='channel')
+    parser.add_argument('-d', '--distance', type=float, default=CFG.dist, help='distance')
+    parser.add_argument('-p', '--power', type=float, default=CFG.power, help='power')
+    parser.add_argument('-R', '--rx_power', type=float, default=None)
+    parser.add_argument('-P', '--plot', action='store_true', default=False)
     
     args = parser.parse_args()
 
     VERBOSE = args.verbose
 
-    Dist  = args.distance
-    Pwr   = args.power
-    Ch    = args.channel
+    Ch = args.channel
 
-    Pt    = CalcAtten(Ch,Dist,Pwr)
+    cal_dist  = args.distance
+    cal_power = args.power
 
-    if DAI(Dist,FC[Ch],Pt) != Pwr:
+    Ptx = CalcTxPower(Ch,cal_dist,cal_power)
+    
+    if DAI(cal_dist,FC[Ch],Ptx) != cal_power:
         raise ValueError
 
-    print('Channel   : {}'.format(Ch))
-    print('Distance  : {:.3f}m'.format(Dist))
-    print('Power     : {:.1f}dBm'.format(Pwr))
-    print('Atten     : {:.1f}dBm'.format(Pt))
+    print('Channel  : {}'.format(Ch))
+    print('CalDist  : {:.3f}m'.format(cal_dist))
+    print('CalPower : {:.1f}dBm'.format(cal_power))
+    print('TxPower  : {:.1f}dBm'.format(Ptx))
 
-    if args.cal is not None:
-        cal_dis = args.cal
-        cal_pwr = DAI(cal_dis,FC[Ch],Pt)
-        print('Calib:    : {:.1f}dBm @{:.3f}m'.format(cal_pwr,cal_dis))
-    
-    Plot(Ch,Pt)
+    if args.rx_power is not None:
+        if args.rx_power > 0.0:
+            rx_dist  = args.rx_power
+            rx_power = DAI(rx_dist,FC[Ch],Ptx)
+        else:
+            rx_power = args.rx_power
+            rx_dist  = ADI(rx_power,FC[Ch],Ptx)
+        print('RxPower  : {:.1f}dBm @{:.3f}m'.format(rx_power,rx_dist))
+
+    if args.plot:
+        Plot(Ch,Ptx)
 
 
 if __name__ == "__main__":
