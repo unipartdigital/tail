40777cd840596841af64cd02a09e59eb1e876e7b
Luke Barone-Adesi <luke.barone-adesi@unipart.io>
2019-06-04 17:46:13 +0100

    added header

--- firmware/src/uart.h
+++ firmware/src/uart.h
@@ -12,6 +12,7 @@ void write_int64(uint64_t n);
 void write_hex(uint32_t n);
 void write_hexdigit(int n);
 void uart_init(void);
+void uart_idle(void);
 bool uart_tx(uint8_t data);
 bool uart_rx(uint8_t *data);
 bool uart_prepare_sleep(void);
